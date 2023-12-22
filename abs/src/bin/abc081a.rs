cb929f93815a875f5fc635e53b5f1f99ae3d83bb
roku36 <roku36000@gmail.com>
2023-12-22 16:59:06 +0900

    prct

--- abs/src/bin/abc081a.rs
+++ abs/src/bin/abc081a.rs
@@ -2,6 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        s: String,
     }
+
+    // check how many digits have "5"
+    let mut count = 0;
+    for c in s.chars() {
+        if c == '1' {
+            count += 1;
+        }
+    }
+    println!("{}", count);
 }
--- abs/src/bin/abc086a.rs
+++ abs/src/bin/abc086a.rs
@@ -2,6 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        
+        a: usize,
+        b: usize,
+    }
+    // a*b even or odd
+    if (a * b) % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
     }
 }
